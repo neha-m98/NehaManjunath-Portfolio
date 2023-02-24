62e096566457054bd92b94a7bc3fe43074cee45f
neha-m98 <66993152+neha-m98@users.noreply.github.com>
2023-02-24 17:40:21 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -39,7 +39,7 @@ class About extends Component {
                     <br></br>
                   </span>
                   <span>
-                    <a href="mailto:neham1998@gmail.com">{email}</a>
+                    <a href="mailto:nehamanjunath98@gmail.com">{email}</a>
                   </span>
                 </p>
               </div>
